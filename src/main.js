fcaf26ba2d8f0a2e1df3148691a77ab125d528c4
zhangyue <dlpuzhangyue@163.com>
2020-01-25 16:11:50 +0800

    项目建立

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import store from './store'
 import xhr from '../src/xhr'
 import 'normalize.css/normalize.css'
 import flexible from 'flexible.js'
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
 Vue.config.productionTip = false
 flexible();
 Vue.use(xhr)
+Vue.use(Vant);
+
 
 new Vue({
   router,
